d05403df9c9e2d095848befd970e6298bd516e33
Ruslan Basyrov <rbasyrov@c3r4p4.42vienna.com>
2023-09-05 19:51:25 +0200

    desperate try to make the criterion compile with the tests

---
+++ lib/criterion/README.md
@@ -0,0 +1,3 @@
+### Note
+
+The binary files were downloaded from https://github.com/Snaipe/Criterion/releases since students at 42 School Vienna do not have sudo rights to install criterion in the usual way. The version is 2.4.2. 
--- tests/tests.h
+++ tests/tests.h
@@ -1,7 +1,7 @@
 #ifndef TESTS_H
 # define TESTS_H
 
-# include <criterion/criterion.h>
+# include </nfs/homes/rbasyrov/42_School_VS_Code/minirt/lib/criterion/include/criterion/criterion.h>
 # include "tuples.h"
 # include "colors.h"
 # include "canvas.h"
