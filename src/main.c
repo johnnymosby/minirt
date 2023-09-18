59b39a12d90112713ef361cfde1fa7d953d7b3d6
Annie <annie.guilmeau@gmail.com>
2023-09-18 16:22:01 +0200

    modified/corrected main with hooks

--- src/main.c
+++ src/main.c
@@ -2,13 +2,42 @@
 #include "tuples.h"
 #include "colors.h"
 #include "canvas.h"
+#include "X11/keysym.h"
 
 #define	ERR_MEMORY_ALLOCATION	-1
 #define	ERR_MLX_FUNCTION		-2
 
+static int	escape_key(int keysym, t_win *win)
+{
+	if (keysym == XK_Escape)
+	{
+		if (win->menu.img_ptr)
+			mlx_destroy_image(win->mlx_ptr, win->menu.img_ptr);
+		if (win->pct.img_ptr)
+			mlx_destroy_image(win->mlx_ptr, win->pct.img_ptr);
+		if (win->win_ptr)
+			mlx_destroy_window(win->mlx_ptr, win->win_ptr);
+		mlx_loop_end(win->mlx_ptr);
+	}
+	return (0);
+}
+
+static int	close_button(t_win *win)
+{
+	if (win->menu.img_ptr)
+		mlx_destroy_image(win->mlx_ptr, win->menu.img_ptr);
+	if (win->pct.img_ptr)
+		mlx_destroy_image(win->mlx_ptr, win->pct.img_ptr);
+	if (win->win_ptr)
+		mlx_destroy_window(win->mlx_ptr, win->win_ptr);
+	mlx_loop_end(win->mlx_ptr);
+	return (0);
+}
+
 int	main(void)
 {
-	t_canvas 	*c = NULL;
+	t_canvas	*c;
+	t_win		win;
 	int 		width = 500;
 	int 		height = 1000;
 	t_color		blue = color(0, 0, 1);
@@ -31,9 +60,13 @@ int	main(void)
 		write_pixel(c, 50, i, red);
 		i++;
 	}
-
-//	INITIALISE MLX	
-	t_win win = window();
+//	INITIALISE MLX
+	win = window();
+	if (win.mlx_ptr == NULL)
+	{	
+		free_canvas(c);
+		return (ERR_MLX_FUNCTION);
+	}
 	if (initialise_picture(&win) == false)
 	{
 		free_canvas(c);
@@ -44,9 +77,16 @@ int	main(void)
 		free_canvas(c);
 		return (ERR_MLX_FUNCTION);
 	}
-	
+//	MLX HOOKS
+	mlx_hook(win.win_ptr, 2, 1L << 0, &escape_key, &win);
+	mlx_hook(win.win_ptr, 17, 1L << 17, &close_button, &win);
+//	RENDERING IMAGE
+
+//	MLX LOOP
+	mlx_loop(win.mlx_ptr);
 //	FREE EVERYTHING
 	free_canvas(c);
-	free_mlx_memory(&win);
+	mlx_destroy_display(win.mlx_ptr);
+	free(win.mlx_ptr);
 	return (0);
 }
