f5dcf2a4c88fe4dbc71082f0e584834775b2c26b
Annie <annie.guilmeau@gmail.com>
2023-09-27 08:49:50 +0200

    changes to scene intersect

--- tests/scene.c
+++ tests/scene.c
@@ -6,18 +6,17 @@
 /*   By: aguilmea <aguilmea@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 12:41:44 by aguilmea          #+#    #+#             */
-/*   Updated: 2023/09/24 15:34:25 by aguilmea         ###   ########.fr       */
+/*   Updated: 2023/09/27 08:46:17 by aguilmea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.h"
 
-
-
 Test(scene, the_default_world)
 {
-//	t_world		w;
+	t_world		w;
 
-//	w = world();
+	w = default_world();
+	(void)w;
 	cr_assert(true);
 }
