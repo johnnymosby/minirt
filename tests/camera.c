f47636f17efa9986a67d31ef8fb54f8fb84e7e2d
Annie <annie.guilmeau@gmail.com>
2023-10-01 19:45:21 +0200

    camera work in progress

--- tests/camera.c
+++ tests/camera.c
@@ -6,7 +6,7 @@
 /*   By: aguilmea <aguilmea@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 10:53:19 by aguilmea          #+#    #+#             */
-/*   Updated: 2023/10/01 11:30:19 by aguilmea         ###   ########.fr       */
+/*   Updated: 2023/10/01 19:40:51 by aguilmea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,26 @@
 Test(constructing_a_camera, camera)
 {
 	t_camera	c;
-	t_matrice	mat;
+	t_matrix	mat;
 	
-	c = camera(160, 120, PI/2);
+	c = camera(160, 120, M_PI_2);
 	mat = identity_matrix();
 	
 	cr_assert(c.hsize == 160);
 	cr_assert(c.vsize == 120);
-	cr_assert(c.field_of_view = PI/2);
+	cr_assert(c.field_of_view = M_PI_2);
 	cr_assert(are_equal_matrices(&mat, &c.transform));	
+
+}
+Test(pixel_size, camera)
+{
+	t_camera	c;
+	
+//	c = camera(200, 125, M_PI_2);
+//	printf("1 - camera.pixel_size %f\n", c.pixel_size);
+//	cr_assert(c.pixel_size == 0.01);
+
+	c = camera(125, 200, M_PI_2);
+	printf("2 - camera.pixel_size %f\n", c.pixel_size);
+	cr_assert(c.pixel_size == 0.01);
 }
