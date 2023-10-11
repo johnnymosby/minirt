4d55437d2481c102194baa69cf6e787835f508b6
Annie Guilmeau <aguilmea@c3r1p1.42vienna.com>
2023-10-11 18:46:17 +0200

    added tests

--- tests/parser_full_files.c
+++ tests/parser_full_files.c
@@ -6,7 +6,7 @@
 /*   By: aguilmea <aguilmea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 14:14:28 by aguilmea          #+#    #+#             */
-/*   Updated: 2023/10/11 18:20:15 by aguilmea         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:45:25 by aguilmea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,37 @@ Test(parser, full_files_subject)
 
 	ret = parser("../testfiles/full_files/subject_only_one_sphere.rt", &w, &c);
 	cr_assert(ret == true);
-	if (w->lightning.material)
-		free(w->lightning.material);
-	if (w->lights)
-		free(w->lights);
-	if (w->shape)
-		free(w->shape);
+	if (w.lightning.material)
+		free(w.lightning.material);
+	if (w.lights)
+		free(w.lights);
+	if (w.shape)
+		free(w.shape);
 
-	//cr_assert(parser("../testfiles/full_files/subject_only_one_cylinder.rt", w, c) == true);
-	//cr_assert(parser("../testfiles/full_files/subject_only_one_plane.rt", w, c) == true);
-	//cr_assert(parser("../testfiles/full_files/subject.rt", w, c) == true);
+	ft_bzero(&w, sizeof(t_world));
+	cr_assert(parser("../testfiles/full_files/subject_only_one_cylinder.rt", &w, &c) == true);
+	if (w.lightning.material)
+		free(w.lightning.material);
+	if (w.lights)
+		free(w.lights);
+	if (w.shape)
+		free(w.shape);
+
+	ft_bzero(&w, sizeof(t_world));
+	cr_assert(parser("../testfiles/full_files/subject_only_one_plane.rt", &w, &c) == true);
+	if (w.lightning.material)
+		free(w.lightning.material);
+	if (w.lights)
+		free(w.lights);
+	if (w.shape)
+		free(w.shape);
+
+	ft_bzero(&w, sizeof(t_world));
+	cr_assert(parser("../testfiles/full_files/subject.rt", &w, &c) == true);
+	if (w.lightning.material)
+		free(w.lightning.material);
+	if (w.lights)
+		free(w.lights);
+	if (w.shape)
+		free(w.shape);
 }
