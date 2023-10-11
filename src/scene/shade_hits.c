e40567dccac15913c3e2cabe68776411b593cc69
Annie Guilmeau <aguilmea@c3r1p1.42vienna.com>
2023-10-11 15:55:05 +0200

    changes to src

--- src/scene/shade_hits.c
+++ src/scene/shade_hits.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   shade_hits.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aguilmea <aguilmea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:14:35 by aguilmea          #+#    #+#             */
-/*   Updated: 2023/10/11 13:01:26 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:27:59 by aguilmea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "computations.h"
-
+/*
 static t_tuple	adjust_point_to_mitigate_acne_effect(t_comp *comps)
 {
 	t_tuple	normalv_by_epsilon;
 
 	normalv_by_epsilon = multiply_tuple_by_scalar(&comps->normalv, EPSILON);
 	return (add_tuples(&comps->point, &normalv_by_epsilon));
-}
+}*/
 
 t_color	shade_hit(t_world *world, t_comp *comps)
 {
 	t_lightning	l;
 
 	ft_bzero(&l, sizeof(t_lightning));
-	comps->over_point = adjust_point_to_mitigate_acne_effect(comps);
+	//comps->over_point = adjust_point_to_mitigate_acne_effect(comps);
 	l.in_shadow = is_shadowed(world, &comps->over_point);
 	l.material = &comps->object.material;
 	l.light = world->lights;
