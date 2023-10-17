8fabfa804074e58fc69f96afdcb61474e178113f
Annie Guilmeau <aguilmea@c3r1p1.42vienna.com>
2023-10-17 11:34:10 +0200

    corrected segfaults while exiting

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aguilmea <aguilmea@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: aguilmea <aguilmea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 21:43:54 by aguilmea          #+#    #+#             */
-/*   Updated: 2023/10/16 19:37:20 by aguilmea         ###   ########.fr       */
+/*   Updated: 2023/10/17 11:28:53 by aguilmea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,44 +67,36 @@ int	pressed_key(int keycode, t_controls *controls)
 
 static int	quit_scene(t_scene *scene, int err_code)
 {
-	printf("a\n");
-	if (scene && scene->canvas)
-	{	
-		printf("b1\n");
-		if (scene->canvas->win)
+	if (scene && scene->canvas && scene->canvas->win)
+	{
+		if (scene->canvas->win->menu.img_ptr != NULL)
+			mlx_destroy_image(scene->canvas->win->mlx_ptr, scene->canvas->win->menu.img_ptr);
+		if (scene->canvas->win->pct.img_ptr)
+			mlx_destroy_image(scene->canvas->win->mlx_ptr, scene->canvas->win->pct.img_ptr);
+		if (scene->canvas->win->win_ptr)
+			mlx_destroy_window(scene->canvas->win->mlx_ptr, scene->canvas->win->win_ptr);
+		if (scene->canvas->win->mlx_ptr)
 		{
-			printf("c1\n");
-			if (scene->canvas->win->menu && scene->canvas->win->menu.img_ptr)
-				mlx_destroy_image(scene->canvas->win->mlx_ptr, scene->canvas->win->menu.img_ptr);
-			printf("c2\n");
-			if (scene->canvas->win->pct.img_ptr)
-				mlx_destroy_image(scene->canvas->win->mlx_ptr, scene->canvas->win->pct.img_ptr);
-			printf("c3\n");
-			if (scene->canvas->win->win_ptr)
-				mlx_destroy_window(scene->canvas->win->mlx_ptr, scene->canvas->win->win_ptr);
-			printf("c4\n");
-			if (scene->canvas->win->mlx_ptr)
-			{
-				mlx_destroy_display(scene->canvas->win->mlx_ptr);
-				free(scene->canvas->win->mlx_ptr);
-			}
-			printf("c2\n");
+			mlx_destroy_display(scene->canvas->win->mlx_ptr);
+			free(scene->canvas->win->mlx_ptr);
 		}
-		free_canvas(scene->canvas);
-		printf("c3\n");
 	}
+	if (scene && scene->canvas)
+		free_canvas(scene->canvas);
 	if (scene && scene->world && scene->world->lightning.material)
 		free (scene->world->lightning.material);
 	if (scene && scene->world && scene->world->lights)
 		free(scene->world->lights);
 	if (scene && scene->world && scene->world->shape)
 		free(scene->world->shape);
-//	print_error_parsing(NULL, 0, err_code);
-	ft_putstr_fd("Error\n", STDERR_FILENO);
 	return (err_code);
 }
+
 static void	set_scene(t_scene *scene, t_world *world, t_win *window)
 {
+	ft_bzero(scene, sizeof(t_scene));
+	ft_bzero(world, sizeof(t_world));
+	ft_bzero(window, sizeof(t_win));
 	scene->world = world;
 	scene->canvas = canvas(PCT_WIDTH, WIN_HEIGHT);
 	scene->canvas->win = window;
@@ -118,11 +110,10 @@ int	main(int argc, char **argv)
 	t_controls	controls;
 	int			ret;
 
+	set_scene(&scene, &world, &win);
+	set_controls(&controls, &scene);
 	if (argc != 2)
 		return(quit_scene(NULL, ERR_NB_ARGUMENTS));
-	printf("test\n");
-	set_controls(&controls, &scene);
-	set_scene(&scene, &world, &win);
 	ret = parser(argv[1], &scene);
 	if (ret != 0)
 		return(quit_scene(&scene, ret));
