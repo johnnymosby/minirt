9ddc19efd7c913e472fb7a40a87dc4470c51f2e6
Annie <annie.guilmeau@gmail.com>
2023-10-22 21:30:52 +0200

    parser vector corrected

--- src/parser/parser__tuple.c
+++ src/parser/parser__tuple.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser__tuple.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aguilmea <aguilmea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aguilmea <aguilmea@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 12:47:17 by aguilmea          #+#    #+#             */
-/*   Updated: 2023/10/22 19:43:26 by aguilmea         ###   ########.fr       */
+/*   Updated: 2023/10/22 21:28:21 by aguilmea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,8 @@ static int	check_value_vector(double coord[3])
 		||coord[1] < -1.0 || coord[1] > 1.0
 		||coord[2] < -1.0 || coord[2] > 1.0)
 		return (ERR_VALUE_RANGE);
-	printf("****%f %f, %f\n", coord[0], coord[1], coord[2]);
-	if (sqrt(coord[0] * coord[0] + coord[1] * coord[1]
-		 + coord[2] * coord[2]) != 1)
+	if (!are_equal_doubles(sqrt(coord[0] * coord[0]
+		+ coord[1] * coord[1] + coord[2] * coord[2]), 1))
 		return (ERR_NORM_VECTOR);
 	return (0);
 }
