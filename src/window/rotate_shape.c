c60cf9d87df77bfedbfec4a427732be34416a598
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2023-10-24 01:19:39 +0200

    shapes: fixed rotation

--- src/window/rotate_shape.c
+++ src/window/rotate_shape.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate_shape.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aguilmea <aguilmea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rbasyrov <rbasyrov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:23:34 by rbasyrov          #+#    #+#             */
-/*   Updated: 2023/10/22 13:34:35 by aguilmea         ###   ########.fr       */
+/*   Updated: 2023/10/24 01:18:32 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	rotate_shape(int keycode, t_controls *controls)
 {
 	t_scene		*scene;
 	t_matrix	m_rotated;
+	t_matrix	*m_old_transform;
+	t_matrix	m_new_transform;
 
 	if (controls->shape_in_control->shape_type == SPHERE)
 		return ;
@@ -27,6 +29,8 @@ void	rotate_shape(int keycode, t_controls *controls)
 		m_rotated = rotation_z(M_PI_4 / 4 * (1 - 2 * (keycode == KEY_E)));
 	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
 		m_rotated = rotation_y(M_PI_4 / 4 * (1 - 2 * (keycode == KEY_RIGHT)));
-	set_updated_transform_in_shape(controls, &m_rotated);
+	m_old_transform = &controls->shape_in_control->transform;
+	m_new_transform = multiply_matrices(m_old_transform, &m_rotated);
+	set_transform(controls->shape_in_control, &m_new_transform);
 	rerender(scene);
 }
